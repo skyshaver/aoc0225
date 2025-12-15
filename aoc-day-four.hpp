c7a11891a8f7d239a69ddcbb5684f002dc8b6e39
skyshaver <skyshaver@me.com>
2025-12-15 14:25:39 -0800

    part 2 solved

--- aoc-day-four.hpp
+++ aoc-day-four.hpp
@@ -1,5 +1,92 @@
 #include "utils.hpp"
 
+auto count_rolls(const std::vector<std::string>& rows) -> int {
+    // [row][col]
+    auto start_row = 1;
+    auto start_col = 1;
+
+    auto end_row = rows.size() - 1;
+    auto end_col = rows[0].size() - 1;
+    int sum = 0;
+    for (size_t r = start_row; r < end_row; r++)
+    {
+        for (size_t c = start_col; c < end_col; c++)
+        {
+            if ((rows[r][c]) == '@')
+            {
+                int acc = 0;
+                for (int i : {-1, 0, 1})
+                {
+                    for (int j : {-1, 0, 1})
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            continue;
+                        }
+                        else if (rows[r + i][c + j] == '@')
+                        {
+                            acc++;
+                        }
+                    }
+                }
+                if (acc < 4) 
+                {
+                    sum++;
+                }
+            }
+        }
+    }    
+    return sum;
+}
+
+
+auto count_rolls_and_set_empty_rolls(std::vector<std::string>& rows) -> int {
+    // [row][col]
+    auto start_row = 1;
+    auto start_col = 1;
+
+    auto end_row = rows.size() - 1;
+    auto end_col = rows[0].size() - 1;
+    std::vector<std::pair<int, int>> remove_list;
+    int sum = 0;
+    for (size_t r = start_row; r < end_row; r++)
+    {
+        for (size_t c = start_col; c < end_col; c++)
+        {
+            if ((rows[r][c]) == '@')
+            {
+                int acc = 0;
+                for (int i : {-1, 0, 1})
+                {
+                    for (int j : {-1, 0, 1})
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            continue;
+                        }
+                        else if (rows[r + i][c + j] == '@')
+                        {
+                            acc++;
+                        }
+                    }
+                }
+                if (acc < 4)
+                {
+                    sum++;
+                    remove_list.push_back({ r, c });
+                }
+            }
+        }
+    }
+
+    for (const auto& e : remove_list)
+    {
+        rows[e.first][e.second] = '.';
+    }
+
+    return sum;
+}
+
 auto aoc_day_four_main() -> void
 {
     auto path = "./test-inputs/day-four.txt";
@@ -18,38 +105,20 @@ auto aoc_day_four_main() -> void
         row.insert(row.end(), '.');
     }
 
-    auto start_x = 1;
-    auto start_y = 1;
-
-    auto end_x = rows[0].size() - 1;
-    auto end_y = rows.size() - 1;
-
-    int sum = 0;
-    for (size_t y = start_y; y < end_y; y++)
-    {
-        for (size_t x = start_x; x < end_x; x++)
-        {
-            if ((rows[y][x]) == '@')
-            {
-                int acc = 0;
-                for (int i : {-1, 0, 1})
-                {
-                    for (int j : {-1, 0, 1})
-                    {
-                        if (i == 0 && j == 0)
-                        {
-                            continue;
-                        }
-                        else if (rows[y + i][x + j] == '@')
-                        {
-                            acc++;
-                        }
-                    }
-                }
-                if (acc < 4)
-                    sum++;
-            }
-        }
-    }
+    // 1505 is the correct answer for part 1
+    auto sum = count_rolls(rows);
     std::cout << "sum:" << sum << '\n';
+
+    
+    auto total = 0;
+    auto s = 0;
+    while ((s = count_rolls_and_set_empty_rolls(rows)) != 0)
+    {
+        total += s;
+    }
+
+    std::cout << "total for part 2: " << total << '\n';
+    
+
+
 }
